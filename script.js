1c648fe1ad5168d9928aa3706e6192fbf3e5980f
TitanService <126705709+Paul1502@users.noreply.github.com>
2024-09-08 16:57:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Lebendiges Logo, das auf die Mausbewegung reagiert
 document.addEventListener('mousemove', (event) => {
   const logo = document.querySelector('.logo');
-  const x = (window.innerWidth / 2 - event.pageX) / 20;
-  const y = (window.innerHeight / 2 - event.pageY) / 20;
+  const x = (window.innerWidth / 2 - event.pageX) / 30;
+  const y = (window.innerHeight / 2 - event.pageY) / 30;
 
-  logo.style.transform = `translate(${x}px, ${y}px)`;
+  logo.style.transform = `translate(${x}px, ${y}px) scale(1.05)`;
 });
 
 // Button Explosion Animation bei Klick
@@ -12,12 +12,14 @@ const discordButton = document.querySelector('.discord-button');
 
 discordButton.addEventListener('click', function(e) {
   const buttonRect = this.getBoundingClientRect();
-  const particles = document.createElement('div');
-  particles.classList.add('particles');
-  particles.style.top = `${e.clientY - buttonRect.top}px`;
-  particles.style.left = `${e.clientX - buttonRect.left}px`;
-  this.appendChild(particles);
-  setTimeout(() => {
-    particles.remove();
-  }, 1000);
+  for (let i = 0; i < 30; i++) {
+    const particles = document.createElement('div');
+    particles.classList.add('particles');
+    particles.style.top = `${e.clientY - buttonRect.top}px`;
+    particles.style.left = `${e.clientX - buttonRect.left}px`;
+    this.appendChild(particles);
+    setTimeout(() => {
+      particles.remove();
+    }, 1000);
+  }
 });
